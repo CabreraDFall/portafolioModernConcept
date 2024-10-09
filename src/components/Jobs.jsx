68599bc5c69c33095de72e0ba4866c7aa66b3649
CabreraDFall <testingdfall@gmail.com>
2024-10-09 11:32:30 +0200

    Jobs Experience Update

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -3,26 +3,67 @@ import Job from "../utilities/Job";
 
 import SectionTitle from "../utilities/SectionTitle";
 
+const jobExperience = [
+  {
+    title: "Data Analyst & CRM Specialist",
+    company: "Ipanema Comunicación",
+    duration: "sept. 2023 - mar. 2024",
+    country: "España",
+    logo: "/public/images/jobsLogo/ipanema_logo.jpg",
+  },
+  {
+    title: "Becario de proyectos",
+    company: "multiAnau Facility Services",
+    duration: "feb. 2023 - ago. 2023",
+    country: "España",
+    logo: "/public/images/jobsLogo/multianau_logo.jpg",
+  },
+  {
+    title: "WordPress Develover",
+    company: "Fall in Code",
+    duration: "abr. 2021 - oct. 2022",
+    country: "República Dominicana",
+    logo: "/public/images/jobsLogo/AC_logo.jpeg",
+  },
+  {
+    title: "Coordinador de producción",
+    company: "Sanut",
+    duration: "ago. 2020 - feb. 2021",
+    country: "República Dominicana",
+    logo: "/public/images/jobsLogo/sanut_logo.jpg",
+  },
+  {
+    title: "Encargado de Redes Sociales",
+    company: "Hospital Regional Dr. Antonio Musa",
+    duration: "sept. 2019 - dic. 2020",
+    country: "República Dominicana",
+    logo: "/public/images/jobsLogo/musa_logo.png",
+  },
+  {
+    title: "Técnico de TI",
+    company: "Hospital Regional Dr. Antonio Musa",
+    duration: "sept. 2018 - dic. 2019",
+    country: "República Dominicana",
+    logo: "/public/images/jobsLogo/musa_logo.png",
+  },
+];
 const Jobs = () => {
   return (
     <div className="w-2/4 rounded c-box h-[400px] overflow-hidden relative flex flex-col gap-3 ">
       <div className=" mx-5 mt-8  flex h-[80%] flex-col gap-5 ">
         <SectionTitle title={"Experiencia"} />
         <div className="flex flex-col gap-5 overflow-y-auto px-5   h-full">
-          <Job />
-          <Job />
-          <Job />
-          <Job />
-          <Job />
+          <Job jobExperience={jobExperience} />
         </div>
       </div>
       <div className="text-c-text  justify-center mb-2 gap-5 h-10% flex items-center  w-full">
-        <div className="fit-content px-5 mt-1 flex rounded  c-box">
-          <span className="text-c-white">Descargar</span>
-        </div>
         <div className="fit-content px-5 mt-1 flex rounded gap-3 c-box">
-          <Linkedin />
-          <span className="text-c-white">LinkedIn</span>
+          <a
+            href="https://www.linkedin.com/in/abraham-cabrera/"
+            className="text-c-white"
+          >
+            LinkedIn
+          </a>
         </div>
       </div>
     </div>
--- src/utilities/Job.jsx
+++ src/utilities/Job.jsx
@@ -1,25 +1,26 @@
-const Job = () => {
+const Job = ({ jobExperience }) => {
   return (
-    <div className="flex w-full items-center">
-      <div className="w-1/5">
-        <img
-          srcSet="/public/images/jobs1.png"
-          className="flex items-center h-full w-full pr-5"
-        />
-      </div>
-      <div className="text-c-white flex flex-col flex-1">
-        <div className="flex justify-between">
-          <h3 className="w-1/2 font-semibold">
-            {" "}
-            Data Analyst & CRM Specialist
-          </h3>
-          <h4 className="text-sm">sept. 2023 - mar. 2024 </h4>
+    <>
+      {jobExperience.map((job, index) => (
+        <div key={index} className="flex w-full items-center">
+          <div className="w-1/5 rounded-sm pr-5">
+            <img
+              srcSet={job.logo}
+              className="flex items-center h-full w-full rounded-sm "
+            />
+          </div>
+          <div className="text-c-white flex flex-col flex-1">
+            <div className="flex justify-between">
+              <h3 className="w-1/2 font-semibold">{job.title}</h3>
+              <h4 className="text-xs">{job.duration} </h4>
+            </div>
+            <span className="text-xs text-[#AEAEAE]">
+              {job.company}, {job.country}
+            </span>
+          </div>
         </div>
-        <span className="text-sm text-[#AEAEAE]">
-          Ipanema Comunicación, España
-        </span>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
